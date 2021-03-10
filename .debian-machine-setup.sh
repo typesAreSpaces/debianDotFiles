c1c379d9308f9ab85e9fd99c5cb5ff85bdb28a73
jose <jabelcastellanosjoo@unm.edu>
2021-03-10 16:05:38 -0700

    Fix debian-machine-setup script.

--- .debian-machine-setup.sh
+++ .debian-machine-setup.sh
@@ -3,7 +3,7 @@
 sudo apt install zsh git vim curl python3-neovim i3 i3-wm i3lock i3status rofi htop autoconf automake autotools-dev g++ g++-7 gcc gcc-7 gdb python3 python2.7 openssh-server
 
 echo ".cfg" >> .gitignore
-git clone --bare https://github.com/typesAreSpaces/archDotFiles.git $HOME/.cfg
+git clone --bare https://github.com/typesAreSpaces/debianDotFiles.git $HOME/.cfg
 mkdir -p .config-backup && \
   /usr/bin/git/ --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
   xargs -I{} mv {} .config-backup/{}
